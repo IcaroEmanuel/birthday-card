70dc5586dda260ee06d49aeba4ae9b1413dc60c1
icaroemanuel <icaro.emanuel@outlook.com>
2021-09-12 14:52:27 -0300

    Criei a estrutura da página

--- src/components/Birthday.js
+++ src/components/Birthday.js
@@ -4,7 +4,17 @@ export default class Birthday extends Component{
  render(){
   return (
     <div>
-
+      <header>
+        <h1>Feliz aniversário</h1>
+      </header>
+      <section>
+        <p>
+          Parabéns, hoje é seu aniversário!!!
+        </p>
+      </section>
+      <section>
+        <button type="button">Mostrar mensagem</button>
+      </section>
     </div>
   );
  }
