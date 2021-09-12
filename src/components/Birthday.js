7d3715069f70f4de746ce0e90db4b4650ed49c45
icaroemanuel <icaro.emanuel@outlook.com>
2021-09-12 15:06:21 -0300

    Criei a lógica do controle de estados

--- src/components/Birthday.js
+++ src/components/Birthday.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
 
 export default class Birthday extends Component{
+  constructor() {
+    super()
+
+    this.state = {
+      showMessage: false,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ showMessage: true });
+  }
+  
  render(){
+   const { showMessage } = this.state;
   return (
     <div>
       <header>
         <h1>Feliz aniversário</h1>
       </header>
-      <section>
-        <p>
-          Parabéns, hoje é seu aniversário!!!
-        </p>
-      </section>
-      <section>
-        <button type="button">Mostrar mensagem</button>
-      </section>
+      { showMessage &&
+        <section>
+          <p>
+            Parabéns, hoje é seu aniversário!!!
+          </p>
+        </section>
+
+      }
+      {
+        !showMessage &&
+        <section>
+          <button type="button" onClick={this.handleClick}>Mostrar mensagem</button>
+        </section>
+      }
     </div>
   );
  }
